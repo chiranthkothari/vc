d61e8abfec309b6dfcc99337f09c25fe2ee54852
Chiranth Kothari <chiranthkothari96@gmail.com>
2017-11-08 15:13:13 +0530

    New

---
+++ assets/js/app.js
@@ -0,0 +1,59 @@
+function bus1(){
+    var a= Number(document.getElementById('bus1').textContent);
+    a++;
+    document.getElementById('bus1').innerHTML= a;
+}
+
+function car1(){
+    var a= Number(document.getElementById('car1').textContent);
+    a++;
+    document.getElementById('car1').innerHTML= a;
+}
+
+function rick1(){
+    var a= Number(document.getElementById('rick1').textContent);
+    a++;
+    document.getElementById('rick1').innerHTML= a;
+}
+
+function bike1(){
+    var a= Number(document.getElementById('bike1').textContent);
+    a++;
+    document.getElementById('bike1').innerHTML= a;
+}
+
+function oth1(){
+    var a= Number(document.getElementById('oth1').textContent);
+    a++;
+    document.getElementById('oth1').innerHTML= a;
+}
+
+function bus2(){
+    var a= Number(document.getElementById('bus2').textContent);
+    a++;
+    document.getElementById('bus2').innerHTML= a;
+}
+
+function car2(){
+    var a= Number(document.getElementById('car2').textContent);
+    a++;
+    document.getElementById('car2').innerHTML= a;
+}
+
+function rick2(){
+    var a= Number(document.getElementById('rick2').textContent);
+    a++;
+    document.getElementById('rick2').innerHTML= a;
+}
+
+function bike2(){
+    var a= Number(document.getElementById('bike2').textContent);
+    a++;
+    document.getElementById('bike2').innerHTML= a;
+}
+
+function oth2(){
+    var a= Number(document.getElementById('oth2').textContent);
+    a++;
+    document.getElementById('oth2').innerHTML= a;
+}
